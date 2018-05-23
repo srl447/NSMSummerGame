dfd7ce2bbee520cdf2ca4118f730796a28b468d2
Unknown <srl447@nyu.edu>
2018-05-23 17:52:12 -0400

    Why no Work?
    Collision Broken

---
+++ NSMGame/Assets/Scripts/SamScript/SamShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamShoot : MonoBehaviour {
+
+    public GameObject bullet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject newBullet = Instantiate(bullet) as GameObject;
+            newBullet.transform.position = this.transform.position;
+        }
+		
+	}
+}
