9dd59cd69c530c61f7e59e9579aeb46e696b730c
Unknown <srl447@nyu.edu>
2018-05-25 12:55:18 -0400

    Chris's Lesson stuff

---
+++ NSMGame/Assets/Scripts/SamScript/BulletForward.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletForward : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(new Vector2(1, 0));
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Box")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
---
+++ NSMGame/Assets/Scripts/SamScript/SamMove2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamMove2 : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate ()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector2(speed, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector2(-speed, 0));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector2(0, speed));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector2(0, -speed));
+        }
+    }
+}
