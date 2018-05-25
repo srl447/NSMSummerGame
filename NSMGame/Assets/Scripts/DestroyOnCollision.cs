37f49f2c4686385c9d770f81a40aa0c300396929
Paul Merritt <pdm309@nyu.edu>
2018-05-25 00:08:22 -0400

    Prototyped enemy shooting AI - enemies fire at the player's position every 5 seconds - bullets are destroyed upon leaving the camera

---
+++ NSMGame/Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        print("Collided! coll: " + coll.gameObject.name);
+        if (coll.gameObject.tag == "Bullet")
+        {
+            Destroy(coll.gameObject); //destroy bullet if it touched colliders on camera's edges
+        }
+    }
+}
