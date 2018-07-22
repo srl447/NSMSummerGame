950a7b1c73f31087ab308c9d40ee8a182cc67e26
Unknown <srl447@nyu.edu>
2018-07-22 15:24:58 -0400

    Jump adding

--- NSMGame/Assets/Scripts/Jumping.cs
+++ NSMGame/Assets/Scripts/Jumping.cs
@@ -2,41 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jumping : MonoBehaviour {
+public class Jumping : MonoBehaviour
+{
 
     public int jumpMultiplier = 12;
     public int gravityMultiplier = 5;
     public float gravity = -9.81f;
 
+    public LayerMask DefaultRaycastLayers;
+
+    public Vector2 raycastStart;
+    public Vector3 raycastAdd;
     Rigidbody2D rb;
 
-     void Awake()
+    public RaycastHit2D jumpCheck;
+
+    void Awake()
     {
-        
+
     }
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        Debug.Log(rb.velocity.y);
-        if (Input.GetKeyDown(KeyCode.W) && rb.velocity.y == 0)
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(GetComponent<SpriteRenderer>().flipX == true)
         {
-            rb.velocity += Vector2.up * jumpMultiplier;
-        }
-        else if (rb.velocity.y > 3)
-        {
-            Physics2D.gravity = new Vector2(0, gravity * gravityMultiplier);
-        }
-        else if (rb.velocity.y < -.5)
-        {
-            Physics2D.gravity = new Vector2(0, gravity * gravityMultiplier * 2);
+            raycastAdd = new Vector3(-1, 1, 0);
         }
         else
         {
-            Physics2D.gravity = new Vector2(0, gravity);
+            raycastAdd = new Vector3(1, 1, 0);
         }
-	}
+        raycastStart = transform.position - raycastAdd;
+        jumpCheck = Physics2D.Raycast(raycastStart, Vector2.down, 2f, DefaultRaycastLayers);
+        Debug.DrawRay(raycastStart, Vector2.down * 2f, Color.green);
+        Debug.Log(jumpCheck.collider.tag);
+        Debug.Log(raycastStart);
+        //Debug.Log(rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.W) && jumpCheck.collider.tag == "Floor")
+        {
+            rb.velocity += Vector2.up * jumpMultiplier;
+        }
+        else if (rb.velocity.y > 6)
+        {
+            //Physics2D.gravity = new Vector2(0, gravity * gravityMultiplier);
+            rb.gravityScale = gravityMultiplier;
+        }
+        else if (rb.velocity.y < -1)
+        {
+            //Physics2D.gravity = new Vector2(0, gravity * gravityMultiplier * 2);
+            rb.gravityScale = gravityMultiplier * 2;
+        }
+        else
+        {
+            //Physics2D.gravity = new Vector2(0, gravity);
+            rb.gravityScale = 1;
+        }
+    }
 }
